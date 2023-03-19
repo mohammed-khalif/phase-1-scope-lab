0b4bbf89b6a150091d9788d02809d8efdb64d2fc
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-03-19 16:17:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "jane";
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because you can't reassign a constant variable
+  leastFavoriteCustomer = "jim";
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer,
+};
